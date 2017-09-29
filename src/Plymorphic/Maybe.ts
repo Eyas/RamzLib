d061c044b3cd0949ae5481df9da0b89aaf86c11b
Eyas <eyas.sharaiha@gmail.com>
2017-09-28 22:41:42 -0400

    Make tslint happy with Maybe test.

--- src/Plymorphic/Maybe.ts
+++ src/Plymorphic/Maybe.ts
@@ -9,16 +9,16 @@ export function getOrElse<T>(m: Maybe<T>, els: T): T {
         return m;
     } else return els;
 }
-export function map<T, R>(m: Maybe<T>, f: (m: T)=>R): Maybe<R> {
+export function map<T, R>(m: Maybe<T>, f: (m: T) => R): Maybe<R> {
     if (hasValue<T>(m)) {
         return f(m);
     } else return m;
 }
-export function flatMap<T, R>(m: Maybe<T>, f: (m: T)=>Maybe<R>): Maybe<R> {
+export function flatMap<T, R>(m: Maybe<T>, f: (m: T) => Maybe<R>): Maybe<R> {
     if (hasValue<T>(m)) {
         return f(m);
     } else return m;
 }
-export function filter<T>(m: Maybe<T>, f: (m: T)=>boolean): Maybe<T> {
+export function filter<T>(m: Maybe<T>, f: (m: T) => boolean): Maybe<T> {
     return hasValue<T>(m) && f(m) ? m : undefined;
 }
